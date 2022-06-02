a47a24da71da07a674bd1f6c45b6187e84f1190a
Lucas Rodrigues <theluks21@live.com>
2022-06-02 15:08:57 -0300

    Feat: adiciona handler de erro genérico

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { errorHandler } = require('../middlewares');
 const { LoginRoutes, RegisterRoutes } = require('../routes');
 require('express-async-errors');
 
@@ -10,5 +11,7 @@ app.use('/login', LoginRoutes);
 app.use('/register', RegisterRoutes);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(errorHandler);
 
 module.exports = app;
