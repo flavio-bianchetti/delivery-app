a1ac8031ed3fb01db810a9f052a9cfd9b5db1760
Allan Melo <allan-m-silva@hotmail.com>
2022-06-03 16:03:53 -0300

    fix: remove console.logs adicionais

--- back-end/src/services/productService.js
+++ back-end/src/services/productService.js
@@ -3,10 +3,8 @@ const { Product } = require('../database/models');
 const getAll = async () => {
   try {
     const products = await Product.findAll();
-    console.log(products);
     return products;
   } catch (err) {
-    console.log('saiu com erro');
     console.error(err);
     return { error: err.message };
   }
