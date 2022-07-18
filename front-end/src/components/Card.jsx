34b3de9734ad293689006d117c87f89fb6f30b4e
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-18 09:00:11 -0300

    feat:(Card/OrderCard) Layout dos componentes Card e OrderCard
    Detalhes:
    - organizado os elementos dos componentes.
    - incluída estilização dos componentes.

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from './Button';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import IconButton from '@mui/material/IconButton';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 import Input from './Input';
 
 const Card = ({
@@ -12,53 +17,110 @@ const Card = ({
   onClick,
   onChange,
 }) => (
-  <div
+  <Box
     className="card"
+    display="flex"
   >
-    <h3
-      className="card__title"
-      data-testid={ `customer_products__element-card-title-${id}` }
+    <Stack
+      sx={ { width: '200px', height: '250px' } }
+      direction="column"
+      spacing={ 1 }
+      border="1px solid green"
+      borderRadius={ 1 }
     >
-      { name }
-    </h3>
-    <img
-      className="card__image"
-      src={ urlImage }
-      alt={ name }
-      data-testid={ `customer_products__img-card-bg-image-${id}` }
-    />
-    <h4
-      className="card__price"
-      data-testid={ `customer_products__element-card-price-${id}` }
+      <Stack
+        display="flex"
+        spacing={ 1 }
+        padding={ 1 }
+        alignItems="center"
+        justifyContent="center"
+      >
+        <img
+          className="card__image"
+          src={ urlImage }
+          alt={ name }
+          height="150px"
+          data-testid={ `customer_products__img-card-bg-image-${id}` }
+        />
+
+      </Stack>
+      <Stack
+        backgroundColor="#f1f1f1"
+        direction="column"
+      >
+        <Stack
+          paddingBottom={ 1 }
+        >
+          <Typography
+            variant="inherit"
+            component="div"
+            align="center"
+            color="green"
+          >
+            { name }
+          </Typography>
+        </Stack>
+        <Stack
+          display="flex"
+          direction="row"
+          width="100%"
+          height="100%"
+          paddingBottom={ 1 }
+
+        >
+          <IconButton
+            aria-label="remove"
+            color="error"
+            name="-"
+            id={ id }
+            onClick={ onClick }
+            datatestid={ `customer_products__button-card-rm-item-${id}` }
+            disabled={ quantity === 0 }
+          >
+            <RemoveCircleIcon />
+          </IconButton>
+          <Input
+            className="card__input"
+            variant="outlined"
+            size="small"
+            type="text"
+            value={ quantity }
+            onChange={ onChange }
+            datatestid={ `customer_products__input-card-quantity-${id}` }
+          />
+          <IconButton
+            aria-label="add"
+            color="success"
+            name="+"
+            id={ id }
+            onClick={ onClick }
+            datatestid={ `customer_products__button-card-add-item-${id}` }
+            disabled={ false }
+          >
+            <AddCircleIcon />
+          </IconButton>
+        </Stack>
+      </Stack>
+    </Stack>
+    <Stack
+      display="flex"
+      position="absolute"
+      margin={ 0.5 }
+      padding={ 0.5 }
+      backgroundColor="white"
+      color="green"
+      borderRadius={ 1 }
     >
-      { `${price.replace('.', ',')}` }
-    </h4>
-    <Button
-      className="navbar__btn-decrement"
-      id={ `${id}` }
-      type="button"
-      onClick={ onClick }
-      datatestid={ `customer_products__button-card-rm-item-${id}` }
-      label="-"
-      disabled={ quantity === 0 }
-    />
-    <Input
-      className="card__input"
-      type="number"
-      value={ quantity }
-      onChange={ onChange }
-      datatestid={ `customer_products__input-card-quantity-${id}` }
-    />
-    <Button
-      className="navbar__btn-increment"
-      id={ `${id}` }
-      type="button"
-      onClick={ onClick }
-      datatestid={ `customer_products__button-card-add-item-${id}` }
-      label="+"
-      disabled={ false }
-    />
-  </div>
+      <Typography
+        variant="h6"
+        component="div"
+        align="center"
+      >
+        { `R$ ${price.replace('.', ',')}` }
+      </Typography>
+    </Stack>
+
+  </Box>
 );
 
 Card.propTypes = {
--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -2,33 +2,106 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import { useNavigate } from 'react-router-dom';
+import { Stack, Box, Typography } from '@mui/material';
 
 const OrderCard = ({ sale }) => {
-  const { id, status, saleDate, totalPrice } = sale;
+  const { id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber } = sale;
   const cleanedDate = moment(Date.parse(saleDate)).format('DD/MM/YYYY');
   const cleanedPrice = totalPrice.replace('.', ',');
 
   const navigate = useNavigate();
 
   return (
-    <button
-      type="button"
-      onClick={ () => navigate(`/customer/orders/${id}`) }
+    <Box
+      display="flex"
     >
-      <span data-testid={ `customer_orders__element-order-id-${id}` }>
-        {`Pedido ${id}`}
-        {' '}
-      </span>
-      <div data-testid={ `customer_orders__element-delivery-status-${id}` }>
-        { status }
-      </div>
-      <div data-testid={ `customer_orders__element-order-date-${id}` }>
-        { cleanedDate }
-      </div>
-      <div data-testid={ `customer_orders__element-card-price-${id}` }>
-        { `R$ ${cleanedPrice}`}
-      </div>
-    </button>
+      <Stack
+        width="320px"
+        height="120px"
+        margin={ 1 }
+        border="2px solid green"
+        borderRadius={ 2 }
+        direction="row"
+        onClick={ () => navigate(`/customer/orders/${id}`) }
+      >
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          padding={ 2 }
+        >
+          <Typography
+            variant="inherit"
+            component="div"
+            align="center"
+            color="green"
+            data-testid={ `customer_orders__element-order-id-${id}` }
+          >
+            { `Pedido ${id}` }
+          </Typography>
+        </Stack>
+        <Stack
+          direction="column"
+          backgroundColor="#f1f1f1"
+        >
+          <Stack
+            direction="row"
+          >
+            <Stack>
+              <Typography
+                component="div"
+                align="center"
+                color="white"
+                backgroundColor="red"
+                padding={ 2 }
+                margin={ 2 }
+                borderRadius={ 1 }
+                data-testid={ `customer_orders__element-delivery-status-${id}` }
+              >
+                { status }
+              </Typography>
+            </Stack>
+            <Stack
+              direction="column"
+            >
+              <Stack>
+                <Typography
+                  component="div"
+                  align="center"
+                  color="green"
+                  padding={ 1 }
+                  data-testid={ `customer_orders__element-order-date-${id}` }
+                >
+                  { cleanedDate }
+                </Typography>
+              </Stack>
+              <Stack>
+                <Typography
+                  component="div"
+                  align="center"
+                  color="green"
+                  padding={ 1 }
+                  data-testid={ `customer_orders__element-card-price-${id}` }
+                >
+                  { `R$ ${cleanedPrice}`}
+                </Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+          <Stack>
+            <Typography
+              component="div"
+              fontSize={ 12 }
+              marginLeft={ 1 }
+              align="justify"
+              color="green"
+              data-testid={ `customer_orders__element-card-address-${id}` }
+            >
+              { `Endereço: ${deliveryAddress}, nº ${deliveryNumber}` }
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
   );
 };
 
@@ -38,6 +111,8 @@ OrderCard.propTypes = {
     status: PropTypes.string.isRequired,
     saleDate: PropTypes.string.isRequired,
     totalPrice: PropTypes.string.isRequired,
+    deliveryAddress: PropTypes.string.isRequired,
+    deliveryNumber: PropTypes.string.isRequired,
   }).isRequired,
 };
 
